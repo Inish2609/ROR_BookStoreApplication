0459b0e38e20c9371d7971527c3361e7e91f3c77
Inish2609 <inishraj.b.s003@gmail.com>
2024-03-12 22:19:56 +0530

    Added Render Settings

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  get 'login',to:'sessions#new'
+  get 'signup',to:'sessions#signup'
+  post 'signup',to:'sessions#create_user'
+  post 'login',to:'sessions#create'
+  delete 'logout',to:'sessions#destroy'
+  resources :books
+  get '/books/filter/:category',to:'books#filter',as:"filter"
+  get 'profile/:id',to:'pages#profile',as:'profile'
+  get 'profile/:id/edit',to:'pages#edit',as:"profileEdit"
+  patch 'profile/:id',to:'pages#update'
+  get 'cart/:id',to:'pages#cart',as:'cart'
+  post 'books/:id',to:'pages#add_cart'
+  get 'about',to:'pages#about'
+  delete 'item/:id',to:'carts#destroy',as:"item"
+  delete 'profile/:id',to:'users#destroy'
+  resources :payments
+  get 'search',to:'books#search'
+end
